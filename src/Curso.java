914c934a81806111abcec533f48a6fad77ff358b
flavio-git <flaviovicentini1991@gmail.com>
2022-09-27 11:22:48 -0300

    aula 6.1 finalizada

--- src/Curso.java
+++ src/Curso.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Curso {
     private String nome;
     private String instrutor;
     private List<Aula> aulas = new ArrayList<>();
+    private Set<Aluno> alunos = new HashSet<>();
 
     public Curso(String nome, String instrutor, List<Aula> aulas) {
         this.nome = nome;
@@ -21,7 +20,7 @@ public class Curso {
         return instrutor;
     }
 
-    // devolve uma cópia do ArrayList e não permite o acesso aos métodos do ArrayList
+    // Devolve uma cópia do ArrayList e não permite o acesso aos métodos do ArrayList
     // é uma boa prática devolver uma cópia... não permitindo o acesso direto ao Array
     public List<Aula> getAulas() {
         return Collections.unmodifiableList(aulas);
@@ -31,12 +30,21 @@ public class Curso {
         this.aulas.add(aula);
     }
 
-    public int getTempoTotalAula(){
+    public int getTempoTotalAula() {
         return this.aulas.stream().mapToInt(Aula::getTempo).sum(); // método que veio com o Java 8
     }
 
+    public void matricula(Aluno aluno){
+        this.alunos.add(aluno);
+    }
+
+    public Set<Aluno> getAlunos() {
+        return Collections.unmodifiableSet(alunos);
+    }
+
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Curso: " + this.nome
                 + " | Instrutor: " + this.instrutor
                 + " | Quantidade aulas: " + this.aulas.size()
--- src/TestandoCursos.java
+++ src/TestandoCursos.java
@@ -15,5 +15,16 @@ public class TestandoCursos {
 
         System.out.println(javaColecoes);
 
+        javaColecoes.matricula(new Aluno("Fernanda", 1020));
+        javaColecoes.matricula(new Aluno("Carlos", 2010));
+        javaColecoes.matricula(new Aluno("Ricardo", 3310));
+        javaColecoes.matricula(new Aluno("Gabriela", 5040));
+        javaColecoes.matricula(new Aluno("Luciana", 5910));
+        javaColecoes.matricula(new Aluno("Bruno", 5911));
+
+        System.out.println("\nAlunos matriculados: ");
+        javaColecoes.getAlunos().forEach(aluno -> {
+            System.out.println(aluno);
+        });
     }
 }
